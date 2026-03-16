a07323c2f9a32d46333865b142d76a288ddcbac2
Omar Reda <or60969@gmail.com>
2026-03-16 17:53:32 +0200

    Added MAE metric to the existing MSE implementation,day_3

--- day_3.py
+++ day_3.py
@@ -15,3 +15,16 @@ y_pred = np.array([2.5, 0.0, 2, 8])
 
 print("MSE:", Mean_Squared_Error(y_pred, y_true))
 # النتيجة المتوقعة: 0.375
+
+def Mean_Absolute_Error(y_pred,y_true):
+    pred=np.asarray(y_pred)
+    gt=np.asarray(y_true)
+
+    if pred.shape != gt.shape:
+        print("Error: Arrays must have the same shape.")
+        return None
+    error=pred-gt
+    abs_error=np.abs(error)
+    MAE=np.mean(abs_error)
+    return MAE
+
